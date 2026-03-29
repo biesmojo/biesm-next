f63492c6767e3d30d6b1d074a535de34de285aeb
Biesmojo <biesmojoaw@gmail.com>
2026-03-29 15:32:33 +0700

    feat: add blog styling and new feeling-lucky page

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,10 +1,12 @@
 import Link from 'next/link';
+import styles from '../../styles/Blog.module.css';
 
 export default function Blog() {
   return (
     <div>
         <h1>Blog</h1>
         <div>
+            <h3 className={styles.red}>Ini adalah halaman blog. Pakai Blog.module.css</h3>
             Back to <Link href="/">Home</Link>
         </div>
     </div>
